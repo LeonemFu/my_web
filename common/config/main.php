fc2f5c3aa7734ec8e2a427f02c542c4a70754db2
herunfu <1340468261@qq.com>
2019-03-18 18:44:47 +0800

    全局数据库配置

--- common/config/main.php
+++ common/config/main.php
@@ -17,7 +17,14 @@ return [
             'enableStrictParsing' => false,
             'rules' => [
                 // ...
-            ],
+            ]
         ],
+		'db' => [
+			'class' => 'yii\db\Connection',
+			'dsn' => 'mysql:host=localhost;dbname=mydb',
+			'username' => 'root',
+			'password' => '',
+			'charset' => 'utf8',
+		],
     ],
 ];
